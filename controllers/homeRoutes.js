6f99b84fa366e9034b8abcd5f4cedebcd1610d07
Faith Vaz <faith.m.vaz@gmail.com>
2023-01-09 20:52:06 -0700

    added more handlebars and routes to render

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,8 +5,24 @@ router.get('/', async (req, res) => {
     res.render('homepage');
 })
 
+router.get('/post', async (req, res) => {
+    res.render('post');
+})
+
+router.get('/comment', async (req, res) => {
+    res.render('comment');
+})
+
 router.get('/dashboard', async (req, res) => {
     res.render('dashboard');
+})
+
+router.get('/dashboard/create', async (req, res) => {
+    res.render('create');
+})
+
+router.get('/dashboard/update', async (req, res) => {
+    res.render('update');
 })
 
 router.get('/login', async (req, res) => {
